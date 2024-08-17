2bf33371ac8feeac0266c5b6cdbcc43da6543b1a
hohaha045 <53906339+hohaha045@users.noreply.github.com>
2024-08-17 19:02:17 +0800

    Update keymap.c

--- keyboards/bastardkb/charybdis/3x5/keymaps/neville/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/neville/keymap.c
@@ -65,21 +65,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
            KC_Q,      KC_W,       KC_E,        KC_R,              KC_T,              KC_Y,      KC_U,        KC_I,       KC_O,              KC_P, \
     SFT_T(KC_A),      KC_S,       KC_D,        KC_F,       ALT_T(KC_G),      RALT_T(KC_H),      KC_J,        KC_K,       KC_L,   RSFT_T(KC_QUOT), \
-           KC_Z,      KC_X,       KC_C,        KC_V,      LGUI_T(KC_B),      RCMD_T(KC_N),      KC_M,     KC_COMM,     KC_DOT,    RCTL_T(KC_ENT), \
+           KC_Z,      KC_X,       KC_C,        KC_V,      LGUI_T(KC_B),      RCMD_T(KC_N),      KC_M,     KC_COMM,     KC_DOT,            KC_ENT, \
                                               RAISE,    RAISE,   LOWER,           KC_BTN1,   KC_BTN2
 ),
 
   [LAYER_RAISE] = LAYOUT( 
     KC_ESC,KC_7,      KC_8,       KC_9,       KC_GRV,          KC_LPRN,           KC_RPRN,    KC_MINS,      KC_EQL,   KC_BSPC,                    \
   SFT_T(KC_TAB),      KC_4,       KC_5,         KC_6,          KC_LALT,            KC_DLR,    KC_AMPR,       KC_AT,   KC_SCLN,   RSFT_T(KC_QUOT), \
-    CTL_T(KC_0),      KC_1,       KC_2,         KC_3,           KC_SPC,           KC_ASTR,    KC_EXLM,     KC_BSLS,   KC_SLSH,    RCTL_T(KC_ENT), \
+    CTL_T(KC_0),      KC_1,       KC_2,         KC_3,           KC_SPC,           KC_ASTR,    KC_EXLM,     KC_BSLS,   KC_SLSH,            KC_ENT, \
                                         XXXXXXX,   XXXXXXX,    _______,           KC_BTN1,   KC_BTN2
 ),
 
   [LAYER_LOWER] = LAYOUT(
            KC_F9,    KC_F10,     KC_F11,       KC_F12,         KC_TILD,           KC_LBRC,    KC_RBRC,    KC_UNDS,    KC_PLUS,           KC_DEL, \
     SFT_T(KC_F5),     KC_F6,      KC_F7,        KC_F8,         KC_LCMD,           KC_LCBR,    KC_RCBR,    KC_HASH,    KC_COLN,          KC_RSFT, \
-    CTL_T(KC_F1),     KC_F2,      KC_F3,        KC_F4,          KC_SPC,           KC_PERC,    KC_CIRC,    KC_PIPE,    KC_QUES,   RCTL_T(KC_ENT), \
+    CTL_T(KC_F1),     KC_F2,      KC_F3,        KC_F4,          KC_SPC,           KC_PERC,    KC_CIRC,    KC_PIPE,    KC_QUES,           KC_ENT, \
                                     _______,        _______,   XXXXXXX,           KC_BTN1,    KC_BTN2
 ),
 
