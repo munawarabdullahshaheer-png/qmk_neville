eaa4c588f5b964ef3b3f23c3ecb4e55f4b2c5a68
hohaha045 <53906339+hohaha045@users.noreply.github.com>
2024-08-17 19:04:35 +0800

    Update keymap.c

--- keyboards/bastardkb/charybdis/3x5/keymaps/neville/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/neville/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT(
            KC_Q,      KC_W,       KC_E,        KC_R,              KC_T,              KC_Y,      KC_U,        KC_I,       KC_O,              KC_P, \
     SFT_T(KC_A),      KC_S,       KC_D,        KC_F,       ALT_T(KC_G),      RALT_T(KC_H),      KC_J,        KC_K,       KC_L,   RSFT_T(KC_QUOT), \
-           KC_Z,      KC_X,       KC_C,        KC_V,      LGUI_T(KC_B),      RCMD_T(KC_N),      KC_M,     KC_COMM,     KC_DOT,            KC_ENT, \
+           KC_Z,      KC_X,       KC_C,        KC_V,      LCMD_T(KC_B),      RCMD_T(KC_N),      KC_M,     KC_COMM,     KC_DOT,            KC_ENT, \
                                               RAISE,    RAISE,   LOWER,           KC_BTN1,   KC_BTN2
 ),
 
