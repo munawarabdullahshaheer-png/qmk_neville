801845cf28352ab67c3631209a1189e125c14a4e
hohaha045 <53906339+hohaha045@users.noreply.github.com>
2024-08-18 15:43:39 +0800

    Update keymap.c

--- keyboards/bastardkb/charybdis/3x5/keymaps/neville/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/neville/keymap.c
@@ -61,20 +61,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 
 // Tap Dance declarations
 enum {
-    TD_DOT_DRG,
-};
-
-// Tap Dance definitions
-tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Escape, twice for Caps Lock
-    [TD_DOT_DRG] = ACTION_TAP_DANCE_DOUBLE(KC_DOT, DRGSCRL),
-};
-
-// Add tap dance item to your keymap in place of a keycode
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    // ...
-    TD(TD_DOT_DRG)
-    // ...
+    DOT_DRG,
 };
 
 // clang-format off
@@ -325,6 +312,61 @@ COMBO(raise_lower_layer_dual, MO(LAYER_DUAL)),
 COMBO(btn1_btn2_btn3, KC_BTN3),
 COMBO(dot_ent_drgscrl, DRGSCRL),
 
+};
+
+//Tap Dance
+typedef struct {
+    uint16_t tap;
+    uint16_t hold;
+    uint16_t held;
+} tap_dance_tap_hold_t;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    tap_dance_action_t *action;
+
+    switch (keycode) {
+        case TD(DOT_DRG):  // list all tap dance keycodes with tap-hold configurations
+            action = &tap_dance_actions[QK_TAP_DANCE_GET_INDEX(keycode)];
+            if (!record->event.pressed && action->state.count && !action->state.finished) {
+                tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
+                tap_code16(tap_hold->tap);
+            }
+    }
+    return true;
+}
+
+void tap_dance_tap_hold_finished(tap_dance_state_t *state, void *user_data) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+
+    if (state->pressed) {
+        if (state->count == 1
+#ifndef PERMISSIVE_HOLD
+            && !state->interrupted
+#endif
+        ) {
+            register_code16(tap_hold->hold);
+            tap_hold->held = tap_hold->hold;
+        } else {
+            register_code16(tap_hold->tap);
+            tap_hold->held = tap_hold->tap;
+        }
+    }
+}
+
+void tap_dance_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+
+    if (tap_hold->held) {
+        unregister_code16(tap_hold->held);
+        tap_hold->held = 0;
+    }
+}
+
+#define ACTION_TAP_DANCE_TAP_HOLD(tap, hold) \
+    { .fn = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}), }
+
+tap_dance_action_t tap_dance_actions[] = {
+    [DOT_DRG] = ACTION_TAP_DANCE_TAP_HOLD(KC_DOT, DRGSCRL),
 };
 
 
