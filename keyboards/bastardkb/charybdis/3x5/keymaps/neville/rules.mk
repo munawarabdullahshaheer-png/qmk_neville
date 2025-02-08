b9a6afa9e0c532202a561bbe1136c7273bd57b23
hohaha045 <53906339+hohaha045@users.noreply.github.com>
2025-02-09 03:39:43 +0800

    Update rules.mk

--- keyboards/bastardkb/charybdis/3x5/keymaps/neville/rules.mk
+++ keyboards/bastardkb/charybdis/3x5/keymaps/neville/rules.mk
@@ -3,14 +3,3 @@ MOUSEKEY_ENABLE = yes
 COMBO_ENABLE = yes
 TAP_DANCE_ENABLE = no
 DYNAMIC_TAPPING_TERM_ENABLE = yes
-
-# Pointing device configuration
-POINTING_DEVICE_ENABLE = yes
-POINTING_DEVICE_DRIVER = custom
-CIRQUE_ENABLE = yes
-
-# Add required source files
-SRC += drivers/sensors/cirque_pinnacle.c
-SRC += drivers/sensors/pmw33xx_common.c
-SRC += pointing/cirque_pinnacle_common.c
-SRC += pointing/pointing_device_driver.c
