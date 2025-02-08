0de7f3750d09ced02f08335abd2fd15a946c29a2
hohaha045 <53906339+hohaha045@users.noreply.github.com>
2025-02-09 01:29:46 +0800

    Update rules.mk

--- keyboards/bastardkb/charybdis/3x5/keymaps/neville/rules.mk
+++ keyboards/bastardkb/charybdis/3x5/keymaps/neville/rules.mk
@@ -6,7 +6,7 @@ DYNAMIC_TAPPING_TERM_ENABLE = yes
 
 # Pointing device configuration
 POINTING_DEVICE_ENABLE = yes
-POINTING_DEVICE_DRIVER = pmw3360
+POINTING_DEVICE_DRIVER = custom
 SPLIT_POINTING_ENABLE = yes
 
 # Enable Cirque Trackpad
@@ -14,9 +14,12 @@ CIRQUE_ENABLED = yes
 CIRQUE_ENABLED_I2C = yes
 
 # Add necessary source files
+SRC += drivers/sensors/pmw3360.c
+SRC += drivers/sensors/pmw33xx_common.c
 SRC += drivers/sensors/cirque_pinnacle.c
 SRC += drivers/sensors/cirque_pinnacle_gestures.c
 SRC += drivers/sensors/cirque_pinnacle_i2c.c
+SRC += pointing_device_drivers.c
 
 # Enable I2C
 I2C_DRIVER_ENABLE = yes
