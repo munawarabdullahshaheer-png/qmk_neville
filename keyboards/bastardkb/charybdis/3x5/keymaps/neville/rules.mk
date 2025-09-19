36e0055fed6381247de8779b334cd9444cefb6ba
munawarabdullahshaheer-png <munawarabdullahshaheer@gmail.com>
2025-09-19 09:20:19 +0500

    Update rules.mk

--- keyboards/bastardkb/charybdis/3x5/keymaps/neville/rules.mk
+++ keyboards/bastardkb/charybdis/3x5/keymaps/neville/rules.mk
@@ -1,9 +1,21 @@
+# Enable VIA for remapping
 VIA_ENABLE = yes
+
+# Enable mouse keycodes
 MOUSEKEY_ENABLE = yes
+
+# Enable combo feature
 COMBO_ENABLE = yes
+
+# Disable tap dance
 TAP_DANCE_ENABLE = no
+
+# Allow per-key tapping term
 DYNAMIC_TAPPING_TERM_ENABLE = yes
-POINTING_DEVICE_ENABLE = yes
-POINTING_DEVICE_DRIVER = custom
-# optional for debugging
+
+# Enable debugging console (for printf/logging)
 CONSOLE_ENABLE = yes
+
+# Enable trackpad (Cirque Pinnacle I2C)
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
